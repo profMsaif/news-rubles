40872516cf59d6b27d3668a8d786d1f4a98fcb2c
aerokiller3 <kirillceeb@gmail.com>
2022-04-23 22:20:53 +0500

    backend+frontend Рабочий проект

--- URFU_django_project/settings.py
+++ URFU_django_project/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'URFU_django_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'News-rubles',
+        'USER': 'postgres',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
--- mainpage/admin.py
+++ mainpage/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import USD
+from .models import EUR
+from .models import News
+from .models import Forecast
+from .models import Resource
+from .models import Currency
 
-# Register your models here.
+admin.site.register(USD)
+admin.site.register(EUR)
+admin.site.register(News)
+admin.site.register(Forecast)
+admin.site.register(Resource)
+admin.site.register(Currency)
