f6fda64f21c16abafb32ec85795dacd8597f501d
Sjsitt <andreiperschin@yandex.ru>
2022-05-03 18:14:34 +0500

    graph changes

--- mainpage/static/mainpage/js/main.js
+++ mainpage/static/mainpage/js/main.js
@@ -1,61 +1,24 @@
-Highcharts.getJSON(
-  'https://cdn.jsdelivr.net/gh/highcharts/highcharts@v7.0.0/samples/data/usdeur.json',
-  function (data) {
-
-    Highcharts.chart('container-chart', {
+var chart = Highcharts.chart('container-chartt', {
       chart: {
-        zoomType: 'x'
+          type: 'bar'
       },
       title: {
-        text: ''
-      },
-      subtitle: {
-        text: document.ontouchstart === undefined ?
-          'Click and drag in the plot area to zoom in' : 'Pinch the chart to zoom in'
+          text: 'Fruit Consumption'
       },
       xAxis: {
-        type: 'datetime'
+          categories: ['Apples', 'Bananas', 'Oranges']
       },
       yAxis: {
-        title: {
-          text: 'Exchange rate'
-        }
+          title: {
+              text: 'Fruit eaten'
+          }
       },
-      legend: {
-        enabled: false
-      },
-      plotOptions: {
-        area: {
-          fillColor: {
-            linearGradient: {
-              x1: 0,
-              y1: 0,
-              x2: 0,
-              y2: 1
-            },
-            stops: [
-              [0, Highcharts.getOptions().colors[0]],
-              [1, Highcharts.color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
-            ]
-          },
-          marker: {
-            radius: 2
-          },
-          lineWidth: 1,
-          states: {
-            hover: {
-              lineWidth: 1
-            }
-          },
-          threshold: null
-        }
-      },
-
       series: [{
-        type: 'area',
-        name: 'USD to EUR',
-        data: data
+          name: 'Jane',
+          data: [1, 0, 4]
+      }, {
+          name: 'John',
+          data: [5, 7, 3]
       }]
-    });
-  }
-);
+  });
+
