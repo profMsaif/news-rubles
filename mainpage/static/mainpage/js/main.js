db5e547980393227114b96c5e2260af8f2c93a0f
Sjsitt <andreiperschin@yandex.ru>
2022-05-12 17:07:39 +0500

    code cleaning

--- mainpage/static/mainpage/js/main.js
+++ mainpage/static/mainpage/js/main.js
@@ -34,3 +34,81 @@ var chart = Highcharts.chart('container-chartt', {
     },
     series: data_usd.charts_currency.series
 });
+
+// ГРАФИК ДОЛЛАР_РУБЛЬ (ПРИ НАЖАТИИ)
+
+function graph_us_rub(){
+    var chart = Highcharts.chart('container-chartt', {
+          chart: {
+              type: 'line'
+          },
+          title: {
+              text: ''
+          },
+          subtitle: {
+            text: 'Доллар/Рубль'
+        },
+          xAxis: {
+              categories: data_usd.charts_currency.dates_list
+          },
+          yAxis: {
+              title: {
+                  text: 'Exchange rate'
+              }
+          },
+          plotOptions: {
+            line: {
+                marker: {
+                    enabled: false,
+                    symbol: 'circle',
+                    radius: 2,
+                    states: {
+                        hover: {
+                            enabled: true
+                        }
+                    }
+                }
+            }
+        },
+          series: data_usd.charts_currency.series
+      });
+    }
+
+// ГРАФИК ЕВРО_РУБЛЬ (ПРИ НАЖАТИИ)
+
+function graph_eur_rub(){
+    var chart = Highcharts.chart('container-chartt', {
+          chart: {
+              type: 'line'
+          },
+          title: {
+              text: ''
+          },
+          subtitle: {
+            text: 'Евро/Рубль'
+        },
+          xAxis: {
+            categories: data_eur.charts_currency_eur.dates_list_eur
+          },
+          yAxis: {
+              title: {
+                  text: 'Exchange rate'
+              }
+          },
+          plotOptions: {
+            line: {
+                marker: {
+                    enabled: false,
+                    symbol: 'circle',
+                    radius: 2,
+                    states: {
+                        hover: {
+                            enabled: true
+                        }
+                    }
+                }
+            }
+        },                      
+          series: data_eur.charts_currency_eur.series_eur
+      });
+    } 
